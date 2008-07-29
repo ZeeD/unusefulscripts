f1c164325d050951b7f7367b91103151ad9b38f1
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2008-07-29 08:44:29 +0000

    Introduzione di check_requirements.sh + uso in un paio di script

--- EstraiData.sh
+++ EstraiData.sh
@@ -2,6 +2,15 @@
 
 echo "TODO: cercare di farne una versione python" >&2
 
+if [ -z "${1}" ]; then
+    echo "Uso: ${0} IMMAGINE [IMMAGINI]" >&2
+    echo "Rinomina ogni immagine con la propria timestamp" >&2
+fi
+
+if ! check_requirements.sh exiv2 grep cut mv; then
+    exit -1
+fi
+
 for f in "${@}"; do
-    mv "$f" "$(exiv2 "$f"|grep timestamp|cut -c19-37).${f#*.}"
+    mv "$f" "$(exiv2 "$f" | grep timestamp | cut -c19-37).${f#*.}"
 done
