f703285939167dcb55d120588c09c511d76aa08a
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2008-08-19 15:59:12 +0000

    Aggiornamenti vari e pulizia del codice vecchio *WARN* AGGIORNAMENTO DI SICUREZZA   EstraiData.sh poteva distruggere o sovrascrivere file non perfettamente taggati! *WARN* AGGIORNAMENTO DI SICUREZZA

--- trunk/EstraiData.sh
+++ trunk/EstraiData.sh
@@ -11,6 +11,16 @@ if ! check_requirements.sh exiv2 grep cut mv; then
     exit -1
 fi
 
-for f in "${@}"; do
-    mv "$f" "$(exiv2 "$f" | grep timestamp | cut -c19-37).${f#*.}"
+for from in "${@}"; do
+    timestamp="$(exiv2 "$from" 2>/dev/null | grep timestamp | cut -c19-37)"
+    if [ -z "${timestamp}" ]; then
+        echo "Errore: formato file non supportato" >&2
+        exit -2
+    fi
+    to="${timestamp}.${from#*.}"
+    if [ -e "${to}" ]; then
+        echo "Errore: ${to} già presente" >&2
+        exit -3
+    fi
+    mv -i "${from}" "${to}"
 done
