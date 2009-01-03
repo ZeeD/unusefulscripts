eb29cb1444fb685ee1c29ae143aa2b3a3f1032dd
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-01-03 10:29:25 +0000

    Numera:   * Ottimizzato number_len   * Aggiunta opzioni --use-dirname, --pre e --switch   * Fix posizione del numero prima delle directory   * Pulizia commenti in testa
    dot2ps:
      * Introduzione check_requirements
      * Pulizie varie

--- trunk/dot2ps.sh
+++ trunk/dot2ps.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-if [[ -z "${@}" ]]; then
+if [ -z "${@}" ]; then
     echo "Uso: ${0} [f] file.dot [files]" >&2
     exit 1
 fi
 
-if [[ "${1}" == "f" ]]; then
+if ! check_requirements.sh dot; then
+    exit -1
+fi
+
+if [ "${1}" == "f" ]; then
     force="1"
     shift
 fi
 
 for parametro in ${@}; do
-    if [[ ! -e "${parametro}" ]]; then
+    if [ ! -e "${parametro}" ]; then
         echo "Warn! ${parametro}: file not found" >&2
         continue
     fi
-    if [[ "${force}" == "1" || ! -e "${parametro/dot/ps}" ]]; then
+    if [ "${force}" == "1" -o ! -e "${parametro/dot/ps}" ]; then
         dot -Tps -o "${parametro/dot/ps}" "${parametro}"
     else
         echo "Warn! ${parametro/dot/ps}: file already found" >&2
