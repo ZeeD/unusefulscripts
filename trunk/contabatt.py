ef47f95c09bb843600d21d965e8561db39893670
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-02-23 15:20:03 +0000

    Conta per singolo tasto

--- trunk/contabatt.py
+++ trunk/contabatt.py
@@ -4,10 +4,15 @@ def main(stream, encoding):
     d = { 'sinistra': {('\\12qa<z', 'mignolo'): 0, ('3wsx', 'anulare'): 0,
                     ('4edc', 'medio'): 0,  ('56rtfgvb', 'indice'): 0},
             'destra': {('78yuhjnm', 'indice'): 0, ('9ik,', 'medio'): 0,
-                    ('0ol.', 'anulare'): 0, (u"'\xc3\xacp\xc3\xa8+\xc3\xb2\xc3\xa0\xc3\xb9-", 'mignolo'): 0} }
+                    ('0ol.', 'anulare'): 0, ("'\xc3\xacp\xc3\xa8+\xc3\xb2\xc3"
+                            "\xa0\xc3\xb9-".decode('utf8'), 'mignolo'): 0} }
+    d2 = dict((char, 0) for char in ''.join([e[0] for e in d['sinistra']] +
+            [e[0] for e in d['destra']]))
     tot = 0
     for row in stream:
         for char in row.decode(encoding):
+            if char in d2:
+                d2[char] += 1
             for hand in d:
                 for (char_set, finger) in d[hand]:
                     if char in char_set:
@@ -22,6 +27,9 @@ def main(stream, encoding):
             tot_finger = d[hand][filter(lambda k: k[1] == finger, d[hand])[0]]
             print "\t\t%s: %d\t(%5.2f%% mano\t%5.2f%% tot)" % (finger,
                     tot_finger, tot_finger*100./tot_hand, tot_finger*100./tot)
+    print "Divisione per tasto:"
+    for char, num in sorted(d2.items(), key=lambda e:e[1], reverse=True):
+        print "\t%s: %d (%5.2f%%)" % (char, num, num*100./tot)
 
 if __name__ == '__main__':
     from sys import argv, stdin
