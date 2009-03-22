475b7616f37d49a3e2eb55605a94a241aae7a0cb
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-03-22 18:18:40 +0000

    dotdisplay * supporto secondo parametro come implementazione di graphviz da usare * fix tempfile non cancellato in caso d'errore trovaRaggruppamenti * visione esplicita di quante pagine ci saranno in ogni raggruppamento aggiorna_lista_postepay * fix stringa attesa * TODO: rimuovere debug print

--- dotdisplay.sh
+++ dotdisplay.sh
@@ -1,16 +1,42 @@
 #!/bin/sh
 
-if [ -z "${1}" -o -n "${2}" ]; then
-    echo "Uso: ${0} file.dot" >&2
+allowed=(dot neato twopi circo fdp)
+
+function join_() {
+    separator="${1}"
+    echo -n "${2}"
+    shift 2
+    for element in "${@}"; do
+        echo -n "${separator}${element}"
+    done
+}
+
+function in_() {
+    searched_element="${1}"
+    shift
+    for element in "${@}"; do
+        if [ "${searched_element}" = "${element}" ]; then
+            echo "${searched_element}"
+            break
+        fi
+    done
+}
+
+DOT="$(in_ "${2}" "${allowed[@]}")"
+if [ -z "${1}" -o -n "${3}" -o \( -n "${2}" -a -z "${DOT}" \) ]; then
+    echo "Uso: ${0} file.dot [$(join_ \| "${allowed[@]}")]"  >&2
     exit 1
 fi
 
-in="${1}"
-out="$(mktemp -t dotdisplay-XXXXXX)"
+if [ -z "${DOT}" ]; then
+    DOT=dot
+fi
 
-if [ ! -e "${in}" ]; then
-    echo "Error! ${in}: file not found" >&2
+if [ ! -e "${1}" ]; then
+    echo "Error! ${1}: file not found" >&2
     exit 2
 fi
 
-dot -Tsvg -o "${out}" "${in}" && svgdisplay "${out}" >/dev/null && rm "${out}"
+out="$(mktemp -t dotdisplay-XXXXXX)"
+"${DOT}" -Tsvg -o "${out}" "${1}" && svgdisplay "${out}" >/dev/null
+rm "${out}"
