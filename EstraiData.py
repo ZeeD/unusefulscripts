ed82d83ec088a393ad12929e4e0a5b5c5c005cc1
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-04-15 07:29:41 +0000

    Fine porting EstraiData in python

--- EstraiData.py
+++ EstraiData.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def parse_exif_date(string):
-    """Take a string in the format YYYY:MM:DD HH:MM:SS
-    return a datetime.datetime object"""
-    from datetime import datetime
-    YYYY_MM_DD, hh_mm_ss = string.split(' ')
-    (YYYY, MM, DD), (hh, mm, ss) = YYYY_MM_DD.split(':'), hh_mm_ss.split(':')
-    YYYY, MM, DD, hh, mm, ss = map(int, (YYYY, MM, DD, hh, mm, ss))
-    return datetime(year=YYYY, month=MM, day=DD, hour=hh, minute=mm, second=ss)
+from datetime import datetime, timedelta
+from os.path import split, splitext, join, isdir
+import ExifTags
+import Image
+from itertools import groupby, tee, izip_longest
+from optparse import OptionParser
+from os import mkdir, rename
 
-def extract_date(image_filename, tagname='DateTime'):
-    """return a datetime object from the exif information stored into the image
-    which is named image_filename"""
-    supported_tags = { 'DateTime': 306, 'DateTimeOriginal': 36867,
+class MetaData(object):
+    _supported_tags = { 'DateTime': 306, 'DateTimeOriginal': 36867,
             'DateTimeDigitized': 36868 }
-    tag = supported_tags[tagname] # raise exception if not supported
-    import ExifTags
-    assert ExifTags.TAGS[tag] == tagname # maybe it's changed...
-    import Image
-    return parse_exif_date(Image.open(image_filename)._getexif()[tag])
-
-def do_rename(mapping):
-    """take a dict of the type { datetime, path },
-    rename the file in the path accornding to datetime"""
-    for date in sorted(mapping):
-        print "Now I'm gonna mv `%s` to `%s'" % (mapping[date], date)
+    def __init__(self, image_filename, tagname='DateTime'):
+        """construct a datetime object from the exif information stored into the
+        image which is named image_filename"""
+        self.filename = image_filename
+        tag = MetaData._supported_tags[tagname]
+        assert ExifTags.TAGS[tag] == tagname # maybe it's changed...
+        self.datetime = datetime.strptime(
+                Image.open(image_filename)._getexif()[tag], '%Y:%m:%d %H:%M:%S')
+        dirname, basename = split(self.filename)
+        basename, ext = splitext(basename)
+        self.split = dirname, basename, ext
 
 def heuristic_by_date((a, b)):
     """Default heuristic, group by date"""
-    return a.date()
+    return a.datetime.date()
 
 class make_heuristic_by_hour_distance(object):
     """Cool heuristic: group if a was made at most max_hour_interval before b"""
     def __init__(self, max_hour_interval=12):
-        from datetime import timedelta
         self.timedelta = timedelta(hours=max_hour_interval)
         self.returnValue = True
 
     def __call__(self, (a, b)):
-        if (b-a) < self.timedelta:
+        if b is None or (b.datetime-a.datetime) < self.timedelta:
             return self.returnValue
         else:
             self.returnValue = not self.returnValue
@@ -46,16 +43,15 @@ class make_heuristic_by_hour_distance(object):
 
 def group_by(iterable, grouper):
     """*wrong* wrapper around itertools.groupby"""
-    from itertools import groupby, tee, izip
     a, b = tee(iterable)
     next(b)
-    return ((g[0] for g in e[1]) for e in groupby(izip(a, b), grouper))
+    return ((g[0] for g in e[1]) for e in groupby(izip_longest(a, b), grouper))
 
-def extract_prefix(list_of_datetimes):
-    """Extract a prefix, the kind of YYYY-MM-DD+(DD+1)+...
+def get_common_prefix(list_of_datetimes):
+    """Extract a common_prefix, the kind of YYYY-MM-DD+(DD+1)+...
     *YEAH*, it doesn't support sequences between 2 different months, so what?"""
     date = list_of_datetimes.next() # cry if list_of_datetimes is empty
-    ret = [ date.strftime('%Y-%m-%d') ]
+    ret = [ date.strftime('%F') ]
     for next_date in list_of_datetimes:
         assert next_date.day <= date.day + 1
         if next_date.day != date.day:
@@ -63,19 +59,49 @@ def extract_prefix(list_of_datetimes):
             date = next_date
     return ''.join(ret)
 
+def new_file(metadata_object, common_prefix, options):
+    dirname, basename, ext = metadata_object.split
+    hh_mm_ss = metadata_object.datetime.strftime('%T')
+    if options.group_in_directories:
+        central_part = join(common_prefix, hh_mm_ss)
+    else:
+        central_part = common_prefix + ' ' + hh_mm_ss
+    if options.preserve_filename:
+        central_part += ' - ' + basename
+    return join(dirname, central_part) + ext
+
+def new_dir(metadata_object, common_prefix, options):
+    dirname, basename, ext = metadata_object.split
+    return join(dirname, common_prefix)
+
 def main(options, args):
     if options.heuristic:
         heuristic = make_heuristic_by_hour_distance()
     else:
         heuristic = heuristic_by_date
-    for e in group_by(sorted(map(extract_date, args)), heuristic):
-        print extract_prefix(e), ', '.join(map(str, e))
+    # mdos -> MetaData ObjectS
+    for mdos in group_by(sorted(map(MetaData, args), key=lambda i: i.datetime),
+            heuristic):
+        mdos1, mdos2 = tee(mdos)
+        common_prefix = get_common_prefix(mdo.datetime for mdo in mdos1)
+        for mdo in mdos2:
+            if options.group_in_directories:
+                dirname = new_dir(mdo, common_prefix, options)
+                if not isdir(dirname):
+                    if options.verbose:
+                        print 'mkdir %r' % dirname
+                    if not options.test:
+                        mkdir(dirname)
+            filename = new_file(mdo, common_prefix, options)
+            if options.verbose:
+                print 'mv %r %r' % (mdo.filename, filename)
+            if not options.test:
+                rename(mdo.filename, filename)
 
 def parse_options():
     """Parse user options
     return options, args (== OptionParser(...).parse_args())
     """
-    from optparse import OptionParser
     parser = OptionParser(version='%prog 0.1', usage='%prog [OPTIONS] PHOTOS')
     parser.add_option('-d', '--group-in-directories', action='store_true',
             default=False, help='group images in directories, once per day')
@@ -83,8 +109,6 @@ def parse_options():
             help='use heuristic to _g_roup images spanned in various days')
     parser.add_option('-p', '--preserve-filename', action='store_true',
             default=False, help='preserve original filename')
-    #parser.add_option('-r', '--recursive', action='store_true', default=False,
-            #help='Apply recursively also to directories') # doesn't makes sense
     parser.add_option('-t', '--test', action='store_true', default=False,
             help="test only: doesn't actually rename anything")
     parser.add_option('-v', '--verbose', action='store_true', default=False,
