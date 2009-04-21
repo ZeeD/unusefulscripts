af3b21625829aff55789b2e61c37fabf2c73ac68
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-04-21 08:24:19 +0000

    trash.sh * modifiche minori dot2ps * refactoring dot2svn * refactoring

--- trunk/dot2ps.sh
+++ trunk/dot2ps.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
-source utilities.sh # check_requirements
+output_format='ps'
 
-if [ -z "${@}" ]; then
-    echo "Uso: ${0} [f] file.dot [files]" >&2
+source utilities.sh # check_requirements, in_
+
+if [ -z "${*}" -o -n "$(in_ "${1}" '-h' '--help')" ]; then
+    echo 'Uso: '"${0}"' [-f|-h] DOT_FILE [DOT_FILES]' >&2
     exit 1
 fi
 
 check_requirements dot
 
-if [ "${1}" == "f" ]; then
-    force="1"
+force='0'
+if [ -n "$(in_ "${1}" '-f' '--force')" ]; then
+    force='1'
     shift
 fi
 
-for parametro in ${@}; do
-    if [ ! -e "${parametro}" ]; then
-        echo "Warn! ${parametro}: file not found" >&2
+for dot_file in "${@}"; do
+    if [ ! -e "${dot_file}" ]; then
+        echo '[Warn] file not found: `'"${dot_file}"\' >&2
         continue
     fi
-    if [ "${force}" == "1" -o ! -e "${parametro/dot/ps}" ]; then
-        dot -Tps -o "${parametro/dot/ps}" "${parametro}"
-    else
-        echo "Warn! ${parametro/dot/ps}: file already found" >&2
+    output_file="${dot_file/dot/${output_format}}"
+    if [ -e "${output_file}" -a "${force}" != '1' ]; then
+        echo '[Warn] file found: `'"${output_file}"\'' (use -f to force)' >&2
         continue
     fi
+    dot -T"${output_format}" -o "${output_file}" "${dot_file}"
 done
