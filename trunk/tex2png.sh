dcbd1aeed311bd77e5b6987b24137909c9a9bc2b
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-05-30 18:12:11 +0000

    utilities.sh   * add a -q parameter to the 'in_' function
    tex2png.sh
      * refactor of the code
      * add a -h and a -f parameter
      * converted from latex + dvipng to pdflatex + pdfcrop + pdftoppm +
            convert
      * Now the image has no borders guessed

--- trunk/tex2png.sh
+++ trunk/tex2png.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
 
-source utilities.sh # check_requirements
+source utilities.sh # check_requirements, in_
 
-if [ -z "${1}" -o -n "${3}" ]; then
-    echo "Uso: ${0} FORMULA [NOMEFILE]" >&2
+if [ -z "${*}" -o -n "$(in_ "${1}" '-h' '--help')" ]; then
+    echo "Uso: ${0} [-h|-f] FORMULA [NOMEFILE]" >&2
     exit 1
-elif [ -e "${1}.png" ]; then
-    echo "Error! '${1}.png' esistente!" >&2
-    exit 2
+fi
+if in_ -q "${1}" '-f' '--force'; then
+    FORCE=true
+    shift 1
+else
+    FORCE=false
+fi
+if [ -n "${2}" ]; then
+    OUTPUT_FILENAME="${2}"
+else
+    OUTPUT_FILENAME="${1}.png"
+fi
+if [ -e "${OUTPUT_FILENAME}" ]; then
+    if "${FORCE}"; then
+        echo "Warning! '${OUTPUT_FILENAME}' esistente!" >&2
+    else
+        echo "Warning! '${OUTPUT_FILENAME}' esistente!" >&2
+        exit 2
+    fi
 fi
 
-check_requirements pwd mktemp latex dvipng
+check_requirements pwd mktemp pdflatex pdfcrop pdftoppm convert
 
-homeDir="$(pwd)"
-cd "$(mktemp -dt tex2png-XXXXXX)"
-echo '\documentclass{article}
+MYDIR="$(pwd)"
+TMPDIR="$(mktemp -dt tex2png-XXXXXX)"
+cd "${TMPDIR}"
+cat <<TEXSOURCE | pdflatex >/dev/null
+\documentclass{article}
 \pagestyle{empty}
 \usepackage{xspace,amssymb,amsfonts,amsmath,color}
 \usepackage{mathptmx}
 \begin{document}
-$$'${1}'$$
-\end{document}' > file.tex
-latex file > /dev/null
-dvipng -O -24.4cm,-6.6cm -D 300 -z 9 -o file.png file.dvi > /dev/null # -bg Transparent -O -9cm,-6cm
-if [ -n "${2}" ]; then
-    OUT="${homeDir}/${2}"
-else
-    OUT="${homeDir}/${1}.png"
-fi
-mv file.png "$OUT"
-rm -R "$(pwd)"
+\$\$${1}\$\$
+\end{document}
+TEXSOURCE
+pdfcrop 'texput.pdf' 'crop.pdf' >/dev/null
+pdftoppm -gray -freetype yes -aa yes -aaVector yes 'crop.pdf' 'image'
+convert 'image-1.pgm' "${MYDIR}/${OUTPUT_FILENAME}"
+rm -Rf "${TMPDIR}"
