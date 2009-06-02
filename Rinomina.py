6df2ffeed3b7477e9e8da209c3da19a54f177a3a
Vito.DeTullio <Vito.DeTullio@adac4894-a252-0410-a4ec-59fa08bd249f>
2009-06-02 10:00:58 +0000

    Pylint-ed Rinomina.py
    Rinomina.py
      * Added module docstring
      * Removed all global variables
      * Sanitized variable names
      * Splitted all the flow into a group of specialized functions
      * now Rinomina.py it's importable :)

--- Rinomina.py
+++ Rinomina.py
@@ -1,97 +1,113 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''
+Rinomina.py
+-----------
+
+Little script used to rename files in a directory reformatting them in a
+sequence, following a series of rules.
+'''
 
 from optparse import OptionParser, OptionValueError
 from os import chdir, listdir, rename
 from os.path import abspath, basename, curdir, exists, splitext, isfile, isdir
 from warnings import warn
-from Image import open
+from Image import open as im_open
 
 def check_dir(option, opt, value, parser):
-    if isdir(value):
-        parser.values.dir = value
-    else:
-        raise OptionValueError(value + " isn't a directory")
-
-parser = OptionParser(
-    version = '%prog 0.2.5',
-    usage = '''%prog [options] [NUMERI_SCARTATI|...]
-    Mette un numero in ordine crescente davanti ad ogni file
-    della directory specificata ignorando i NUMERI_SCARTATI'''
-)
-parser.add_option('-d', '--dir', action="callback", type='string', default='.',
-        callback=check_dir, help="enumerate files located in DIR (default='.')")
-parser.add_option('-b', '--begin', type="int", default=1, metavar='N',
-        help="start to enumerate from N (default=1)")
-parser.add_option('-v', '--verbose', action='store_true', default=False,
-        help="show on STDOUT what happens")
-parser.add_option('-c', '--cifre', type="int", default=0, metavar='N',
-        help="use at least N chars to represents the numbers")
-parser.add_option('-u', '--use-dirname', action='store_true', default=False,
-        help="use the dir name instead of the original name file")
-parser.add_option('-i', '--imgs', action='store_true', default=False,
-        help="treat files as images, sort then by pixel resolution, not name")
-parser.add_option('-t', '--test', action='store_true', default=False,
-        help="test only: doesn't actually rename anything")
-parser.add_option('-a', '--also-dir', action='store_true', default=False,
-        help="rename also the name of the directories (not recursive!)")
-options, args = parser.parse_args()
-
-try:
-    scarti = set(int(parametro) for parametro in args)
-except ValueError:
-    raise SystemExit('ci sono elementi che non sono numeri, tra gli scarti')
+    '''Input validating function to check if a value it's a directory'''
+    # pylint: disable-msg=W0613
+    class NotADirectory(OptionValueError):
+        '''Wrapper OptionValueError class used to this particular case'''
+        pass
+    if not isdir(value):
+        raise NotADirectory(value)
+    parser.values.dir = value
 
 def sort_images(image_file_name):
-    """Regole: una immagine è più grande di un'altra se la sua superficie è più
-    grande. --> x*y deve essere grande
+    '''Regole: una immagine è più grande di un'altra se la sua superficie è
+    maggiore. --> x*y deve essere grande
     A parità di superficie, si ritiene maggiore quella più quadrata. --> x == y
     --> abs(x-y) deve -> 0 positivamente --> -abs(x-y) deve -> 0 negativame (e
     più grande è meglio è)
-    A parità di diagonale, sono uguali :P
-    """
-    x, y = open(image_file_name).size
-    return (x*y, -abs(x-y))
+    A parità di diagonale, sono uguali :P'''
+    im_x, im_y = im_open(image_file_name).size
+    return (im_x*im_y, -abs(im_x-im_y))
 
-def condition(filename):
-    if options.also_dir:
-        return isfile(filename) or isdir(filename)
-    else:
-        return isfile(filename)
+def mkparser():
+    '''create and return an OptionParser instance with all options added'''
+    ret = OptionParser(version='%prog 0.3', usage='%prog [OPTS] [SCRAP|...]\n'
+            'Rename files with a leading number, except those SCRAP-ped')
+    ret.add_option('-d', '--dir', action="callback", type='string', default='.',
+            callback=check_dir, help="Sort files in DIR (default='%default')")
+    ret.add_option('-b', '--begin', type="int", default=1, metavar='N',
+            help="Start enumerating from N (default=%default)")
+    ret.add_option('-v', '--verbose', action='store_true', default=False,
+            help="Show on STDOUT what happens")
+    ret.add_option('-c', '--cifre', type="int", default=0, metavar='N',
+            help="Use at least N chars to represents the numbers")
+    ret.add_option('-u', '--use-dirname', action='store_true', default=False,
+            help="Use the dir name instead of the original name file")
+    ret.add_option('-i', '--imgs', action='store_true', default=False,
+            help="Files are images: sort then by pixel resolution, not by name")
+    ret.add_option('-t', '--test', action='store_true', default=False,
+            help="Test only: doesn't actually rename anything")
+    ret.add_option('-a', '--also-dir', action='store_true', default=False,
+            help="Rename also the directories (not recursive!)")
+    return ret
 
-chdir(options.dir)
-filePresenti = sorted(filter(condition, listdir('.')), key=sort_images if
-        options.imgs else None, reverse=options.imgs)
+def parse_and_validate_args():
+    '''simple wrapper around mkparser()'''
+    parser = mkparser()
+    options, args = parser.parse_args()
+    scarti = set(int(parametro) for parametro in args)
+    return options, scarti
 
-associa = []
-for file in filePresenti:
-    while options.begin in scarti:
-        options.begin += 1
-    associa.append((file, options.begin))
-    options.begin += 1
+def mk_associations(filenames, begin, scarti):
+    '''return a 2-tuple: (dict(source_name: number), max_used_number_length)'''
+    associations = {}
+    number = begin
+    for filename in filenames:
+        while number in scarti:
+            number += 1
+        associations[filename] = number
+        number += 1
+    return associations, len(str(number-1))
 
-if options.use_dirname:
-    formato = ' - %%.%dd' % max(options.cifre, len(str(associa[-1][1])))
-else:
-    formato = '%%.%dd - ' % max(options.cifre, len(str(associa[-1][1])))
+def main():
+    '''main function'''
+    options, scarti = parse_and_validate_args()
+    chdir(options.dir)
+    filenames = sorted((filename for filename in listdir('.') if
+            isfile(filename) or options.also_dir and isdir(filename)),
+            key=sort_images if options.imgs else None, reverse=options.imgs)
 
-if options.use_dirname:
-    nome_directory = basename(abspath(curdir))
+    associa, max_length = mk_associations(filenames, options.begin, scarti)
+    max_length = max(options.cifre, max_length)
 
-for sorgente, intero in associa:
     if options.use_dirname:
-        destinazione = nome_directory + formato % intero + splitext(sorgente)[1] # nb: assicurarsi che l'estensione ci sia :D
-    else:
-        destinazione = formato % intero + sorgente
-    if not exists(destinazione):
-        if options.verbose:
-            print sorgente, '->', destinazione,
-            if options.imgs:
-                x, y = open(sorgente).size
-                print "(%dx%d, %d, %d)" % (x, y, x*y, -abs(x-y))
-            else:
-                print
-        if not options.test:
-            rename(sorgente, destinazione)
-    else:
-        warn(destinazione+' è già usato!', RuntimeWarning, 2)
+        nome_directory = basename(abspath(curdir))
+    formato = (' - %%.%dd' if options.use_dirname else '%%.%dd - ') % max_length
+
+    for sorgente, intero in associa.items():
+        if options.use_dirname:
+            ext = splitext(sorgente)[1] # NB: be sure there is an extension!
+            destinazione = nome_directory + formato % intero + ext
+        else:
+            destinazione = formato % intero + sorgente
+        if not exists(destinazione):
+            if options.verbose:
+                print sorgente, '->', destinazione,
+                if options.imgs:
+                    im_x, im_y = im_open(sorgente).size
+                    print "(%dx%d, %d, %d)" % (im_x, im_y, im_x*im_y,
+                            -abs(im_x-im_y))
+                else:
+                    print
+            if not options.test:
+                rename(sorgente, destinazione)
+        else:
+            warn(destinazione+' è già usato!', RuntimeWarning, 2)
+
+if __name__ == '__main__':
+    main()
