0f09e4dafee08b8f1999a80f8be3d801e5b16803
Vito.DeTullio@gmail.com <Vito.DeTullio@gmail.com@adac4894-a252-0410-a4ec-59fa08bd249f>
2013-08-04 05:17:29 +0000

    pep8

--- trunk/offset_changer.py
+++ trunk/offset_changer.py
@@ -16,6 +16,7 @@ string
 SPLITTER = ' --> '
 TIME_FMT = '%H:%M:%S,%f'
 
+
 def offset_changer(filename, offset):
     infn = filename + '~'
     outfn = filename
@@ -29,10 +30,16 @@ def offset_changer(filename, offset):
 
             subtitle_time = next(infile).strip()
             start, end = subtitle_time.split(SPLITTER)
-            start, end = datetime.strptime(start, TIME_FMT), datetime.strptime(end, TIME_FMT)
+            start, end = (
+                datetime.strptime(start, TIME_FMT),
+                datetime.strptime(end, TIME_FMT)
+            )
             if (start + offset).year >= 1900:
                 start, end = start + offset, end + offset
-            start, end = start.strftime(TIME_FMT)[:-3], end.strftime(TIME_FMT)[:-3]
+            start, end = (
+                start.strftime(TIME_FMT)[:-3],
+                end.strftime(TIME_FMT)[:-3]
+            )
             subtitle_time = SPLITTER.join([start, end])
             outfile.write(subtitle_time + '\n')
 
@@ -41,6 +48,7 @@ def offset_changer(filename, offset):
                 outfile.write(message)
                 if not message.strip():
                     break
+
 
 def main():
     if len(argv) < 3:
