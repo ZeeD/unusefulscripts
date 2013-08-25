e2826e010a46e1eaf55fcc6aa4efb2120b7bc1f8
Vito.DeTullio@gmail.com <Vito.DeTullio@gmail.com@adac4894-a252-0410-a4ec-59fa08bd249f>
2013-08-25 13:55:54 +0000

    add wine games

---
+++ kaiba.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pushd '/home/zed/Desktop/YuGiOh Power of Chaos Full....extract and play/Yu-Gi-Oh! Power of Chaos/Konami/Yu-Gi-Oh! Power of Chaos KAIBA THE REVENGE'
+wine 'kaiba_pc.exe' '-fps' '-Lita' '-speedy'
+popd
---
+++ torment.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pushd '/home/zed/.wine/drive_c/Games/Planescape Torment'
+wine Torment.exe
+popd
