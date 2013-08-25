1c463eaeaad0fa39ed62ed354fb4fe8cab146aca
Vito.DeTullio@gmail.com <Vito.DeTullio@gmail.com@adac4894-a252-0410-a4ec-59fa08bd249f>
2013-08-25 13:55:54 +0000

    add wine games

---
+++ trunk/joey.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pushd '/home/zed/Desktop/YuGiOh Power of Chaos Full....extract and play/Yu-Gi-Oh! Power of Chaos/Konami/Yu-Gi-Oh! Power of Chaos JOEY THE PASSION'
+wine 'joey_pc.exe' '-fps' '-Lita' '-speedy'
+popd
