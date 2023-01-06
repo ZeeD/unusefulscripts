387921baef71b512b0ecb2f453475b69bc535ca5
Vito De Tullio <vito.detullio@gmail.com>
2023-01-06 09:08:37 +0100

    mv to eclipse, refresh of UnicodeName

--- UnicodeName.py
+++ UnicodeName.py
@@ -1,48 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import sys
-import unicodedata
+from sys import argv
+from unicodedata import category, name
 
-def data(chars):
-    for char in chars:
-        codepoint = ord(char)
-        category = unicodedata.category(char)
-        try:
-            name = unicodedata.name(char)
-        except Exception as e:
-            print('\t\t%s -> %s' % (char, e))
-            continue
-        yield (char, codepoint, category, name)
+if len(argv) < 2:
+    raise SystemExit(f'Uso: {argv[0]} [strings]*')
 
-def c(sys, sienc):
-    for string in sys.argv[1:]:
-        print(u'c  -> string: %r' % (string, ))
-        ustring = string.decode(sienc)
-        print(u'c  -> ustring: %r' % (ustring, ))
-        for char in ustring:
-            print(u'c  -> char: %s' % (char, ))
-            yield char
-
-def main():
-    '''Uso: UnicodeName [strings]*'''
-
-    #fsenc = sys.getfilesystemencoding()
-    sienc = sys.stdin.encoding  # better guess, I guess
-
-    if len(sys.argv) < 2:
-        raise SystemExit(u'Uso: %s [strings]*' % (sys.argv[0].decode(sienc), ))
-
-    from pprint import pprint
-    pprint(sys.argv[1:][0].decode('UTF-8'))
-
-    chars = c(sys, sienc)
-
-    chars = list(chars)
-    pprint(chars)
-
-    for char, codepoint, category, name in data(chars):
-        print(u'%s\t\\u%04x\t%s\t%s' % (char, codepoint, category, name))
-
-if __name__ == '__main__':
-    main()
+for i, string in enumerate(argv[1:]):
+    if i > 0:
+        print()
+    print(f'{string=!r}')
+    for char in string:
+        print(f'{char}\t\\U{ord(char):08x}\t{category(char)}\t{name(char)}')
